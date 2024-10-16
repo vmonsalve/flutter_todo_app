a7b92e424e075642572cd7fab4a2c761cfe5d94d
Vicente Antonio Monsalve Vargas <vicente.monsalve@gmail.com>
2024-10-15 21:01:12 -0300

    todo app, box todo and checkbox

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo_app/util/todo_tile.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List toDoList = [
+    ["Ducharse", true],
+    ["Tomar desayuno", true],
+    ["Revisar datos trading", true],
+    ["Estudiar Flutter", false],
+  ];
+
+  void checkboxBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        backgroundColor: Colors.yellow,
+        title: Text("Todo App"),
+        elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkboxBoxChanged(value, index),
+          );
+        },
+      ),
+    );
+  }
+}
